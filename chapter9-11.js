010aae6f8f5a5db82c9076eeb66eba7a7acee625
Sarim-Siraj <sarimsirajss@gmail.com>
2024-09-21 14:51:34 +0500

    Update chapter9-11.js

--- chapter9-11.js
+++ chapter9-11.js
@@ -38,10 +38,65 @@
 
 
 // Question 4
-var userInput=+prompt("Enter Remaining Fule");
-if(userInput<0.25){
-    document.write("Please refill the Fule in your car")
-}
-else{
-    document.write("Start Your journay")
-}
+// var userInput=+prompt("Enter Remaining Fule");
+// if(userInput<0.25){
+//     document.write("Please refill the Fule in your car")
+// }
+// else{
+//     document.write("Start Your journay")
+// }
+
+
+// Question 5
+// // Part A
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// // Part B
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// Part C
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+// // Part D
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// // Part E
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+// // Part F
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+
+// Question 6
