e50f272d7627be675309299073cf634f8194434c
Sarim-Siraj <sarimsirajss@gmail.com>
2024-09-21 14:42:17 +0500

    update

---
+++ chapter9-11.js
@@ -0,0 +1,47 @@
+// Question 1
+// var userInput=prompt("Enter Your city Name");
+// if(userInput==="Karachi"){
+//     document.write("Welcom to City of Light");
+// }
+// else{
+//     document.write("Your City is normal");
+// }
+
+// Question 2
+// var userInput=prompt("Enter Your Gender");
+// if(userInput==="Male"){
+//     document.write("Good Morning, Sir")
+
+// }
+// else if (userInput==="Female"){
+//     document.write("Good Morning, maam")
+// }
+// else{
+//     document.write("Sorry")
+// }
+
+// Question 3
+// var userInput=prompt("Enter light  Color");
+// if(userInput==="Red"){
+//     document.write("Must Stop");
+
+// }
+// else if (userInput==="Yellow"){
+//     document.write("Ready To Move");
+// }
+// else if (userInput==="Green"){
+//     document.write("Move Now");
+// }
+// else{
+//     document.write("Stop");
+// }
+
+
+// Question 4
+var userInput=+prompt("Enter Remaining Fule");
+if(userInput<0.25){
+    document.write("Please refill the Fule in your car")
+}
+else{
+    document.write("Start Your journay")
+}
